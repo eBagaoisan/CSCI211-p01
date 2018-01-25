eb88176acf1d85e80a1a55ff0f0cdb5abde7da8f
ebagaoisan <ebagaoisan1@mail.csuchico.edu>
2018-01-25 15:44:14 -0800

    First commit

---
+++ chart.cpp
@@ -0,0 +1,23 @@
+// chart.cpp
+// Bagaoisan, ElmerPeter
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    const int MAX = 100;
+    int userNum;
+    int values[MAX];
+    cout<<"Please enter number of asterisk: ";
+    cin>>userNum;
+
+    return 0;
+}
+
+//largest number function
+
+//printing function
+
+
