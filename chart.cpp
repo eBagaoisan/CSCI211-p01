f3ce071546e6a4e5dbbf5daea6403fe6ed12844a
ebagaoisan <ebagaoisan1@mail.csuchico.edu>
2018-01-28 16:52:05 -0800

    Added functions; Fix syntax for inserting numbers

--- chart.cpp
+++ chart.cpp
@@ -3,21 +3,59 @@
 
 #include <iostream>
 #include <string>
+#include <array>
 
 using namespace std;
 
+//Prototypes
+int largest(int big[]&);
+
+//global
+const int MAX = 100;
+int big = 0;
+
+//main function
 int main(){
-    const int MAX = 100;
-    int userNum;
+    int userNum = 1;
     int values[MAX];
-    cout<<"Please enter number of asterisk: ";
-    cin>>userNum;
+    while (userNum != 0){
+        cout<<"Please enter numbers of asterisk: ";
+        cin>>userNum;
+        if(userNum <= MAX){
+            //asdf
+        }
+        else if(userNum >= 0){
+            cout<<"Please enter a positive number."<<endl;
+        }
+        else{
+            cout<<"Number is too large"<<endl;
+        }
+    }
+
+    //puts user number into array
+    for (int i=0; i<userNum; i++){
+
+    }
 
     return 0;
 }
 
 //largest number function
+int largest(int numArr[]){
+    for (int i = 0; i < numArr.size(); i++){
+        if (numArr[i] < big){
+            big = numArr[i];
+        }
+    }
+    return big;
+}
 
-//printing function
+//array insert
+//parameters: value array and user input
+void apply(int arr[], int input){
+    
+}
 
+
+//Printing function
 
